4647efac87d0db321f6e223a606045d6ee415d42
Troll1r <akiralpa@gmail.com>
2021-01-23 18:58:08 +0400

    23.1.2021

---
+++ Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    protected Joystick joystik;
+    void Start()
+    {
+        joystik = FindObjectOfType<Joystick>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystik.Horizontal * 10f,rigidbody.velocity.y,joystik.Vertical * 10f);
+        
+    }
+}
